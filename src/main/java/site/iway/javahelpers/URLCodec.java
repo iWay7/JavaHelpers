a3e31d7bc703801b41d7f51e64c2f27a00e95573
iWay7 <806070587@qq.com>
2018-06-17 10:33:31 +0800

    Add old codes.

---
+++ src/main/java/site/iway/javahelpers/URLCodec.java
@@ -0,0 +1,41 @@
+package site.iway.javahelpers;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+@Deprecated
+public class URLCodec {
+
+    public static String encode(String s) {
+        if (s == null)
+            return null;
+        return URLEncoder.encode(s);
+    }
+
+    public static String encode(String s, String charset) {
+        try {
+            if (s == null)
+                return null;
+            return URLEncoder.encode(charset, charset);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static String decode(String s) {
+        if (s == null)
+            return null;
+        return URLDecoder.decode(s);
+    }
+
+    public static String decode(String s, String charset) {
+        try {
+            if (s == null)
+                return null;
+            return URLDecoder.decode(charset, charset);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
